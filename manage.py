89f2ff130455e597ab33301737b3f6bc6ab8a1c1
DvmeSon <kurakaoosprunk@gmail.com>
2019-12-25 12:27:52 +0100

    commit 3

--- manage.py
+++ manage.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-#!/usr/bin/env python
 from __future__ import absolute_import, unicode_literals
 
 import os
 import sys
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "grindcms.settings.dev")  ##### Replace grindwiki with your project name
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grindcms.settings.dev')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-    main()
